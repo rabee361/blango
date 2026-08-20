2ddee5549be583a67e357d37b08600eced412ef3
Rabee Hasan <rha60540@gmail.com>
2026-08-20 18:43:02 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,13 @@ from blog.models import Post
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 
 logger = logging.getLogger(__name__)
 
+@cache_page(300)
+@vary_on_headers("Cookie")
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
