4548a57113d10ff7d7a9dee515d5427143ff822c
Rabee Hasan <rha60540@gmail.com>
2026-09-25 18:42:52 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,6 +6,12 @@ from blog.api.views import PostList, PostDetail
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
